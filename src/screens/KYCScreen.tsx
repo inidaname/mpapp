6614bce9b7db7eaa7a2a1cde5ea65e73d70a18bf
Hassan Sani <saniyhassan@gmail.com>
2025-12-17 22:06:46 +0100

    fix: update navigation URL check in KycScreen for correct phase transition

--- src/screens/KYCScreen.tsx
+++ src/screens/KYCScreen.tsx
@@ -7,18 +7,18 @@ import { WebView } from "react-native-webview";
 import { FullNavStack } from "../types/types";
 import { useGetKYCQuery } from "../service/endpoints/kyc-endpoints";
 
-interface Props extends NativeStackScreenProps<FullNavStack, "KYCScreen"> {}
+interface Props extends NativeStackScreenProps<FullNavStack, "KYCScreen"> { }
 
 const KycScreen: React.FC<Props> = ({ route, navigation }) => {
   const { kycLink, tosLink } = route.params;
   const { data, refetch } = useGetKYCQuery();
-  const [currentUrl, setCurrentUrl] = useState(tosLink);
-  const [phase, setPhase] = useState<"tos" | "kyc">("tos");
+  const [ currentUrl, setCurrentUrl ] = useState(tosLink);
+  const [ phase, setPhase ] = useState<"tos" | "kyc">("tos");
 
   const handleNavigationStateChange = async (navState: { url: string }) => {
     const { url } = navState;
 
-    if (phase === "tos" && url.startsWith("https://kyc.insfers.com/agreed")) {
+    if (phase === "tos" && url.startsWith("https://kyc.insfers.com/")) {
       await refetch().unwrap();
       setPhase("kyc");
       setCurrentUrl(kycLink);
